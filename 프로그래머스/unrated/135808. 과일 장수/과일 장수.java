35d99c2af986972a3cb10b9de133b4981161812c
choiyeonwook <48903882+choiyounwook@users.noreply.github.com>
2022-12-04 19:39:45 +0900

    [unrated] Title: 과일 장수, Time: 4.48 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,17 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        List<Integer> list = new ArrayList<Integer>();
-        // list에 score의 수 모두 삽입 후 절렬
-        for(int i=0;i<score.length;i++) list.add(score[i]);
-        list.sort(Collections.reverseOrder());
-       // m개의 사과 상자 만들기
+         int answer = 0;
+        Integer[] arr= Arrays.stream(score).boxed().toArray(Integer[]::new);
+        		
+        // score 정렬
+        Arrays.sort(arr, Collections.reverseOrder());
+        // m개의 사과 상자 만들기
         // 마지막 숫자가 가장 작은 숫자
-        for(int i = m;i<=list.size();i+=m) {
-        	answer +=(list.get(i-1)*m);
+        for(int i=m;i<=arr.length;i+=m)
+        {
+        	answer+=(arr[i-1]*m);
         }
-
+      
         return answer;
     }
 }
