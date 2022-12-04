60a092a24b5a9a219f5c8093cb4f943354c5f615
choiyeonwook <48903882+choiyounwook@users.noreply.github.com>
2022-12-04 13:32:18 +0900

    [unrated] Title: 문자열 나누기, Time: 0.49 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int cntT=0;
+        int cntF=0;
+        char x='0';
+        // x 같은 수 = x와 다른 수 일때 분리
+        // 이때 x도 포함해서 카운팅을 한다.
+        for(char c:s.toCharArray())
+        {
+            if(x=='0') x=c;
+            if(x==c) cntT++;
+            else cntF++;
+            
+            if(cntT==cntF)
+            {
+                answer++;
+                cntT = cntF =0;
+                x ='0';
+            }
+        }
+        if(cntT!=cntF)answer++;
+        return answer;
+    }
+}
