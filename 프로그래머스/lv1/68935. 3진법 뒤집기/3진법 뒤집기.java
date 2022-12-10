ace12c24ab42cf61fd004e2e411a6b788a179931
choiyeonwook <48903882+choiyounwook@users.noreply.github.com>
2022-12-10 15:05:18 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,20 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String result="";
+        long answer = 0;
+        int result=0;
+        long flag=0;
         
         while(n!=0) {
-        result+=(n%3);
+        answer*=10;
+        answer+=(n%3);
         n=n/3;
         }
-        return Integer.parseInt(result,3);
+        while(answer!=0)
+        {
+        	result+=(Math.pow(3,flag)*(answer%10));
+        	flag++;
+        	answer/=10;
+        }
+        return result;
     }
 }
