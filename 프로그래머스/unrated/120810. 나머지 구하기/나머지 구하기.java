e34938ff70543cab227afbb50e9f3e5398122554
choiyeonwook <48903882+choiyounwook@users.noreply.github.com>
2023-12-06 13:27:35 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        /**
+		 * 나머지 구하기
+		 * num1을 num2로 나눈 나머지 값 구하기
+		 * */
+        int answer = -1;
+        return num1%num2;
+    }
+}
