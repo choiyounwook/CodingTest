2b88636b12b377dc577b58bfcdb9f2f44dd812fb
choiyeonwook <48903882+choiyounwook@users.noreply.github.com>
2023-12-06 13:25:52 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        /**
+		 * 숫자 비교하기
+		 * 
+		 * 두 수가 같으면 1 다르면 -1
+		 * */
+        int answer = 0;
+        return num1==num2?1:-1;
+    }
+}
