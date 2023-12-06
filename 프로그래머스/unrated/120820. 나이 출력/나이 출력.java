d30af4584997daed69bee798aa48f2ffb92fa3f8
choiyeonwook <48903882+choiyounwook@users.noreply.github.com>
2023-12-06 13:17:42 +0900

    [level unrated] Title: 나이 출력, Time: 0.01 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int age) {
+   	/**
+		 * 나이 출력
+		 * 2022년 기준으로 출생 연도 구하기(2023-나이)
+		 * 최대 나이 120살
+		 * */
+        int answer = 2023;
+        return answer-age;
+    }
+}
