f481da6b3eff2b203343f6784cb14e2c235eca73
choiyounwook <48903882+choiyounwook@users.noreply.github.com>
2024-03-13 13:52:23 +0900

    [level 2] Title: H-Index, Time: 0.37 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,33 +1,35 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         /**
 		 * 프로그래머스 H-index
 		 * 
-		 * n : 발표한 논문의 수 h : h 번 이상 인용된 논문이 h편 이상이고 나머지 논문은 h 번 이하 인용된 것
-		 * 
+		 * n : 발표한 논문의 수 h : h 번 이상 인용된 논문이 h편 이상이고 나머지 논문은 h 번 이하 인용된 것 정렬 사용 시
 		 */
 
 		int answer = 0;
 		int n = citations.length; // 논문 수
 
+		Arrays.sort(citations);
+
 		for (int h = n; h > 0; h--) {
-			// h번 이상 논문 인용
-			int max = 0;
+
 			// h번 이하 인용
 			int min = 0;
-			for (int i = 0; i < citations.length; i++) {
-				if (citations[i] >= h) {
-					max++;
-				} else
+			for (int i = 0; i < n; i++) {
+				if (citations[i] < h) {
 					min++;
+				} else
+					break;
 			}
+			
+			// h번 이상 논문 인용
+			int max = n - min;
 			if (h <= max && h >= min) {
 				answer = h;
 				break;
 			}
-
 		}
-
 		return answer;
     }
 }
